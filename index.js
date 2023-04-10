c9df66ec857f649e71a5e73ad7fb4c996ed4b5e1
Jose Laurito <jigolaurito93@yahoo.com>
2023-04-10 14:31:25 -0500

    register.ejs and added navbar

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-  res.send('Hello World!!!!');
+    console.log('Hello');
+    // render register.ejs and pass in object as parameter
+    res.render('register', {username: 'jlaur'});
 });
 
 app.listen(port, () => {
